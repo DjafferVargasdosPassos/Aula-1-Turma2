4d39e67657c24f6c8f72c234bf01e436be8f141f
djaffer vargas <djaffervargas@gmail.com>
2019-09-10 23:14:41 -0300

    Feito em casa com ajuda do Carlos Tavares, comecei a entender essa parte

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o livro");
+            var numeroDoLivro = Console.ReadLine();
+            if (numeroDoLivro == "123456")
+            {
+                Console.WriteLine(" Livro indisponível");
+                Console.ReadKey();
+                return;                
+            }
+            else
+            {
+                Console.WriteLine(" Deseja alocar livro? 1-sim ou 2-não");
+                var alocarLivro = Console.ReadLine();
+                if (alocarLivro == "1")
+                {
+                    Console.WriteLine(" Livro alocado.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine(" Livro não alocado");
+                    Console.ReadKey();
+                }
+
+            }
+           
+
+        }
+    }
+}
