8d05b2a109ce04cfab70d73ea4584ad8ca58e3e1
djaffer vargas <djaffervargas@gmail.com>
2019-09-25 17:15:32 -0300

    exercicio 25/09 context

---
+++ VendaDeCarros/InterfaceVendas/Controller/CarrosController.cs
@@ -0,0 +1,23 @@
+﻿using InterfaceVendas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceVendas.Controller
+{
+    class CarrosController
+    {
+        private List<Carros> listaDeCarros { get; set; }
+        public CarrosController()
+        {
+            listaDeCarros = new List<Carros>();
+
+            listaDeCarros.Add(new Carros() {Id = 1,Carro = "Risus Company",Valor = 7.200,QuantidadeVendida = 18,DataDaVenda = DateTime.Parse("29/01/2019") });
+            listaDeCarros.Add(new Carros() {Id =  2,Carro = "Risus Associates",Valor = 9.961,QuantidadeVendida = 4,DataDaVenda = DateTime.Parse("10/02/2019")});
+
+        }               
+
+    }
+}
---
+++ VendaDeCarros/InterfaceVendas/Model/Carros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceVendas.Model
+{
+   
+    public class Carros
+    {
+        public int Id { get; set; }
+        public string Carro { get; set; }
+        public double Valor { get; set; }
+        public int QuantidadeVendida { get; set; }
+        public DateTime DataDaVenda { get; set; }
+
+    }
+}
