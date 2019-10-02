e7f8def41daefa7b41c0c81caedfad2699c1e3e0
djaffer vargas <djaffervargas@gmail.com>
2019-10-02 08:34:36 -0300

    01/10/2019 SQL BancoDeDados

---
+++ SQLQuery4.sql
@@ -0,0 +1,19 @@
+﻿select
+car.Modelo, --Coluna da nossa tabela Carros Modelo
+mar.Nome, --Coluna da nossa tabela marcas Nome
+car.Placa,-- Coluna da nossa tabela Carros Placa
+car.Ano -- Coluna da nossa tabela Carro Ano
+from -- Indicador que vamos selecionar uma tabela
+Carros -- Tabela que vamos selecionar
+car-- Abreviamos e criamos um alias para chamar e ficar mais facil
+inner-- Indicador que ambas as tabelas devem estar relacionadas com os dados 
+join --Indicador do banco que junta uma tabela a outra 
+Marcas--Nossa tabela que vamos juntar a Carros
+mar-- abreviação da nossa tabela de Marcas para facilitar as referencias
+on-- indicador que vamos mostrar como vamos relacionar as informações
+car.MarcaCodigo = mar.Codigo --Aqui informamos que carros tem um codigo iguao a=em marcas e portanto a referencia de marca
+--seria a coluna codigo com isso ele nos tras a linha que temos essa referencia  e 
+--podemos utilizar todas as colunas da linha com informação
+
+
+select car.Modelo,mar.Nome,car.Placa,car.Ano from Carros car inner join marcas mar on car.MarcaCodigo = mar.Codigo
---
+++ SQLQuery8.sql
@@ -0,0 +1,65 @@
+﻿create database BaseSistemaLocacao -- criamos nossa base de dados
+GO 
+use BaseSistemaLocacao -- indicamos que estamos utilizando ela para os comandos logo abaixo
+
+GO
+--Criação da nossa primeira tabela no banco de dados
+create table Usuarios
+(
+    [Id]               INT         IDENTITY (1, 1) NOT NULL,
+    [Nome]             NCHAR (100) NOT NULL,
+    [login]            NCHAR (50)  NOT NULL,
+    [Senha]            NCHAR (50)  NOT NULL,
+    [Ativo]            BIT         DEFAULT ((1)) NULL,
+    [UsuarioCriacao]   INT         DEFAULT ((0)) NULL,
+    [UsuarioAlteracao] INT         DEFAULT ((0)) NULL,
+    [DataCriacao]      DATETIME    DEFAULT (getdate()) NULL,
+    [DataAlteracao]    DATETIME    DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+Go
+create table Marcas
+(
+    [Id]     INT        IDENTITY (1, 1) NOT NULL,
+    [Codigo] INT        NULL,
+    [Nome]   NCHAR (20) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+
+GO
+create table Carros
+(
+    [Id]               INT        IDENTITY (1, 1) NOT NULL,
+    [Modelo ]          NCHAR (50) NOT NULL,
+    [MarcaCodigo]      INT        NOT NULL,
+    [Placa]            NCHAR (7)  NOT NULL,
+    [Ano ]             INT        NOT NULL,
+    [Ativo]            BIT        DEFAULT ((1)) NULL,
+    [UsuarioCriacao]   INT        DEFAULT ((0)) NULL,
+    [UsuarioAlteracao] INT        DEFAULT ((0)) NULL,
+    [DataCriacao]      DATETIME   DEFAULT (getdate()) NULL,
+    [DataAlteracao]    DATETIME   DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+GO
+create table Locacao
+(
+    [Id]               INT        IDENTITY (1, 1) NOT NULL,
+    [UsuarioId]        INT        NOT NULL,
+    [CarroId]          NCHAR (10) NOT NULL,
+    [Valor]            MONEY      NOT NULL,
+    [DataInicial]      DATETIME   DEFAULT (getdate()) NOT NULL,
+    [DataFinal]        DATETIME   DEFAULT (dateadd(day,(5),getdate())) NOT NULL,
+    [Ativo]            BIT        DEFAULT ((1)) NULL,
+    [UsuarioCriacao]   INT        DEFAULT ((0)) NULL,
+    [UsuarioAlteracao] INT        DEFAULT ((0)) NULL,
+    [DataCriacao]      DATETIME   DEFAULT (getdate()) NULL,
+    [DataAlteracao]    DATETIME   DEFAULT (getdate()) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
+GO
+insert into Usuarios (Nome,login,Senha) values('Administrador','admin','admin')
+GO
+INSERT INTO [dbo].[Marcas] ([Codigo], [Nome]) VALUES (1, 'Chevrolet')
+GO
+INSERT INTO [dbo].[Marcas] ( [Codigo], [Nome]) VALUES ( 2, 'Volkswagen')
