08ea39f6c210affc881f5ffb0df823b1f9b28c81
djaffer vargas <djaffervargas@gmail.com>
2019-10-08 17:22:11 -0300

    Entity de Cervejas 08-10-2019

---
+++ PudimSolution/CodeFirstTeste/Controller/CervejaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstTeste.Model;
+
+namespace CodeFirstTeste.Controller
+{
+    public class CervejaController
+    {
+        CervejaContextDb contextDb = new CervejaContextDb();
+        public void AddCerveja(Cerveja item)
+        {
+            contextDb.Cervejas.Add(item);
+            contextDb.SaveChanges();
+        }
+    }
+}
---
+++ PudimSolution/GambCodeFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using CodeFirstTeste.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GambCodeFirst
+{
+    class Program
+    {
+        static CervejaContextDb cervejaContext = new CervejaContextDb();
+        static void Main(string[] args)
+        {
+            
+                cervejaContext.Add(new Cerveja()
+                {
+                    Nome = "Cerveja Budweiser"
+                });
+        }
+    }
+}
