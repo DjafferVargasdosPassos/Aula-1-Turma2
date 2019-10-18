00be8b0bbbbdd3587720e1211ea6c99d45db4af0
djaffer vargas <djaffervargas@gmail.com>
2019-10-17 23:26:11 -0300

    commit final

--- WEB/ConversorMoeda/js/script.js
+++ WEB/ConversorMoeda/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready( //aqui ao carregar o documento iniciamos o js
     function () { //este function funciona igual ao new 
-        $('input[type="button"]').click( // adicionamos a funcao ao nosso botão
+        $('button[name="btn"]').click( // adicionamos a funcao ao nosso botão
             function () {
 
                 var meuValorReal = $('input[name="real"]').val().replace(".","").replace(",","."); //aqui obtemos o valor ".val(999)" setamos o valor
@@ -11,3 +11,8 @@ $(document).ready( //aqui ao carregar o documento iniciamos o js
             });
         $('input[name="real"]').mask("000.000,00");
     });
+	$(document).on('keypress', function (e) {
+    if (e.which === 13) {
+        $('button[name="btn"]').click();
+    }
+});
