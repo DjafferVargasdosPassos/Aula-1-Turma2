9bed09f685d6dbcb96e3f90c874b0bc78182788c
djaffer.passos <djaffer.passos@hbsis.com.br>
2020-01-28 17:09:22 -0300

    metodo embaralhamento

---
+++ TesteRandom/TesteRandom/Entidade/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteRandom.Entidade
+{
+    public class Student
+    {      
+        public Student(string name)
+        { Name = name; }
+        public string Name { get ; set ; }
+       
+    }
+}
+
+
+
---
+++ TesteRandom/TesteRandom/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TesteRandom.Entidade;
+
+namespace TesteRandom
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> mylist = new List<Student>();
+
+            mylist.Add(new Student("JAMES"));
+            mylist.Add(new Student("DAVID"));
+            mylist.Add(new Student("PAUL"));
+            mylist.Add(new Student("GEORGE"));
+            mylist.Add(new Student("LARRY"));
+
+            //shuffle
+            var rnd = new Random();
+            var result = mylist.OrderBy(item => rnd.Next());
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
